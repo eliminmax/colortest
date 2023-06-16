fad701018c44f57d4874c8a3a57b1042e533b27e
Eli Array Minkoff <eli@earrayminkoff.tech>
2023-06-16 16:56:03 -0400

    consistently use process.stdout.write

--- colortest.js
+++ colortest.js
@@ -1,13 +1,14 @@
 #!/usr/bin/env node
 const util = require('util');
+
 // Print the first 16 colors - these vary by terminal configuration
-console.log()
+process.stdout.write('\n')
 for(var i = 0; i < 16; i++) {
-    //process.stdout.write(util.format()) is console.log without the newline
+    // process.stdout.write(util.format()) is basically console.log without 
+    // the automatic trailing newline
     process.stdout.write(util.format('\u001b[48;5;%dm  ', i)) 
 }
-console.log('\u001b[0m')
-console.log()
+process.stdout.write('\u001b[0m\n\n')
 
 // Print the 6 sides of the color cube - these are more standardized
 // but the order is a bit odd, thus the need for this trickery
@@ -23,9 +24,9 @@ for(var i = 16; i < 52; i+=6) {
     for(var ii = 72; ii < 78; ii++) {
         process.stdout.write(util.format('\u001b[48;5;%dm  ', i+ii))
     }
-    console.log('\u001b[0m')
+    process.stdout.write('\u001b[0m\n')
 }
-console.log()
+process.stdout.write('\n')
 
 for(var i = 124; i < 160; i+=6) {
     for(var ii = 0; ii < 6; ii++) {
@@ -39,12 +40,12 @@ for(var i = 124; i < 160; i+=6) {
     for(var ii = 72; ii < 78; ii++) {
         process.stdout.write(util.format('\u001b[48;5;%dm  ', i+ii))
     }
-    console.log('\u001b[0m')
+    process.stdout.write('\u001b[0m\n')
 }
-console.log()
+process.stdout.write('\n')
 
 // Finally, the 24 grays
 for(var i = 232; i < 256; i++) {
     process.stdout.write(util.format('\u001b[48;5;%dm  ', i))
 }
-console.log('\u001b[0m\n')
+process.stdout.write('\u001b[0m\n\n')
