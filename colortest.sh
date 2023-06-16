cd367bb205df67da46fb9864142afd5bbd144e58
Eli Array Minkoff <eli@earrayminkoff.tech>
2023-06-16 16:21:58 -0400

    Streamline layout and logic of colortest.sh

--- colortest.sh
+++ colortest.sh
@@ -1,58 +1,51 @@
 #!/bin/sh
+
 # Print the first 16 colors - these vary by terminal configuration
 printf '\n'
-i=0
-while [ $i -lt 16 ]; do
+i=0; while [ $i -lt 16 ]; do
     printf "\033[48;5;%dm  " $i
-    i=$((i + 1))
+    i=$(( i + 1 ))
 done
 printf "\033[0m\n\n"
+
 # Print the 6 sides of the color cube - these are more standardized
 # but the order is a bit odd, thus the need for this trickery
-i=16
-while [ $i -lt 52 ]; do
-    ii=0
-    while [ $ii -lt 6 ]; do
-        printf "\033[48;5;%dm  " $((i + ii))
-        ii=$((ii + 1))
+i=16; while [ $i -lt 52 ]; do
+    ii=0; while [ $ii -lt 6 ]; do
+        printf "\033[48;5;%dm  " $(( i + ii ))
+        ii=$(( ii + 1 ))
     done
     printf "\033[0m  "
-    ii=0
-    while [ $ii -lt 6 ]; do
-        printf "\033[48;5;%dm  " $((i + ii + 36))
-        ii=$((ii + 1))
+    ii=36; while [ $ii -lt 42 ]; do
+        printf "\033[48;5;%dm  " $(( i + ii ))
+        ii=$(( ii + 1 ))
     done
     printf "\033[0m  "
-    ii=0
-    while [ $ii -lt 6 ]; do
-        printf "\033[48;5;%dm  " $((i + ii + 72))
-        ii=$((ii + 1))
+    ii=72; while [ $ii -lt 78 ]; do
+        printf "\033[48;5;%dm  " $(( i + ii ))
+        ii=$(( ii + 1 ))
     done
     printf "\033[0m\n"
-    i=$((i + 6))
+    i=$(( i + 6 ))
 done
 printf '\n'
-i=124
-while [ $i -lt 160 ]; do
-    ii=0
-    while [ $ii -lt 6 ]; do
-        printf "\033[48;5;%dm  " $((i + ii))
-        ii=$((ii + 1))
+i=124; while [ $i -lt 160 ]; do
+    ii=0; while [ $ii -lt 6 ]; do
+        printf "\033[48;5;%dm  " $(( i + ii ))
+        ii=$(( ii + 1 ))
     done
     printf "\033[0m  "
-    ii=0
-    while [ $ii -lt 6 ]; do
-        printf "\033[48;5;%dm  " $((i + ii + 36))
-        ii=$((ii + 1))
+    ii=36; while [ $ii -lt 42 ]; do
+        printf "\033[48;5;%dm  " $(( i + ii ))
+        ii=$(( ii + 1 ))
     done
     printf "\033[0m  "
-    ii=0
-    while [ $ii -lt 6 ]; do
-        printf "\033[48;5;%dm  " $((i + ii + 72))
-        ii=$((ii + 1))
+    ii=72; while [ $ii -lt 78 ]; do
+        printf "\033[48;5;%dm  " $(( i + ii ))
+        ii=$(( ii + 1 ))
     done
     printf "\033[0m\n"
-    i=$((i + 6))
+    i=$(( i + 6 ))
 done
 printf '\n'
 
@@ -60,6 +53,6 @@ printf '\n'
 i=232
 while [ $i -lt 256 ]; do
     printf "\033[48;5;%dm  " $i
-    i=$((i + 1))
+    i=$(( i + 1 ))
 done
 printf '\033[0m\n\n'
