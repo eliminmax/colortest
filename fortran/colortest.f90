b2dd8b9ce08cb5f652399c35cd18d83ebaca2f54
Eli Array Minkoff <eli@planetminkoff.com>
2024-11-12 15:36:55 -0500

    Refactor FORTRAN implementation
    Now uses internal subroutines for color_cell, cube_row, and
    cube_row_part
    
    No longer specifies a kind for integers, as that made adding values
    inline more tedious

--- fortran/colortest.f90
+++ fortran/colortest.f90
@@ -1,62 +1,65 @@
-! SPDX-FileCopyrightText: 2023 Eli Array Minkoff
+! SPDX-FileCopyrightText: 2023-2024 Eli Array Minkoff
 ! SPDX-License-Identifier: GPL-3.0-only
 
 program colortest
   implicit none
   character        :: ESC, NL
-  character(len=7) :: COLORSTART
-  integer(kind=2)  :: i, ii
+  integer          :: I
   character(len=4) :: COLOREND
   ESC = achar(27)
   NL = achar(10)
-  COLORSTART = ESC // "[48;5;" ! Start of almost every escape sequence used
   COLOREND = ESC // "[0m"
 
   ! Print the first 16 colors - these vary by terminal configuration
   write (*, "(A)", advance="no") NL
   do i = 0, 15
-    write (*, "(A, I0, A)", advance="no") COLORSTART, i, "m  "
+    call color_cell(i)
   end do
   write (*, "(A, A, A)", advance="no") COLOREND, NL, NL
 
   ! Print the 6 sides of the color cube - these are more standardized
-  ! but the order is a bit odd, thus the need for this trickery
+  ! but the order is a bit odd, thus the need for the below trickery
   do i = 16, 46, 6
-    do ii = 0, 5
-      write (*, "(A, I0, A)", advance="no") COLORSTART, (i+ii), "m  "
-    end do
-    write (*, "(A, A)", advance="no") COLOREND, "  "
-    do ii = 36, 41
-      write (*, "(A, I0, A)", advance="no") COLORSTART, (i+ii), "m  "
-    end do
-    write (*, "(A, A)", advance="no") COLOREND, "  "
-    do ii = 72, 77
-      write (*, "(A, I0, A)", advance="no") COLORSTART, (i+ii), "m  "
-    end do
-    write (*, "(A, A)", advance="no") COLOREND, NL
+    call cube_row(i)
   end do
   write(*, "(A)", advance="no") NL
   do i = 124, 154, 6
-    do ii = 0, 5
-      write (*, "(A, I0, A)", advance="no") COLORSTART, (i+ii), "m  "
-    end do
-    write (*, "(A, A)", advance="no") COLOREND, "  "
-    do ii = 36, 41
-      write (*, "(A, I0, A)", advance="no") COLORSTART, (i+ii), "m  "
-    end do
-    write (*, "(A, A)", advance="no") COLOREND, "  "
-    do ii = 72, 77
-      write (*, "(A, I0, A)", advance="no") COLORSTART, (i+ii), "m  "
-    end do
-    write (*, "(A, A)", advance="no") COLOREND, NL
+    call cube_row(i)
   end do
   write(*, "(A)", advance="no") NL
 
   ! Finally, the 24 grays
   do i = 232, 255
-    write (*, "(A, I0, A)", advance="no") COLORSTART, i, "m  "
+    call color_cell(i)
   end do
   write(*,"(A, A, A)", advance="no") COLOREND, NL, NL
 
-  ! vi: ft=fortran sw=2 sts=2 ts=2
+contains
+
+  subroutine color_cell(N)
+    integer, intent(in) :: N
+    ! 27 is ASCII escape
+    write(*, "(A, I0, A)", advance="no") ESC // "[48;5;", N, "m  "
+  end subroutine color_cell
+
+  subroutine cube_row_part(N)
+    integer, intent(in) :: N
+    integer :: II ! internal iterator
+    do II = N, N+5
+      call color_cell(II)
+    end do
+    write(*, "(A)", advance="no") ESC // "[0m"
+  end subroutine cube_row_part
+
+  subroutine cube_row(N)
+    integer, intent(in) :: N
+    call cube_row_part(N)
+    write(*, "(A)", advance="no") "  "
+    call cube_row_part(N+36)
+    write(*, "(A)", advance="no") "  "
+    call cube_row_part(N+72)
+    write(*, "(A)", advance="no") NL
+  end subroutine cube_row
 end program colortest
+
+! vi: ft=fortran sw=2 sts=2 ts=2
