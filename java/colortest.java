3d82d8bf9bb7f94b2e6b3d30c30d21c55533e76d
Eli Array Minkoff <eli@planetminkoff.com>
2024-11-12 17:09:47 -0500

    Refactor Java version
    The fact that there's no way to add to a short without a cast is stupid.
    I thought D's inability to do it inline was bad, but at least D makes it
    possible. Suffice to say, I am not fond of Java.

--- java/colortest.java
+++ java/colortest.java
@@ -1,53 +1,38 @@
-// SPDX-FileCopyrightText: 2022 Eli Array Minkoff
+// SPDX-FileCopyrightText: 2022-2024 Eli Array Minkoff
 //
 // SPDX-License-Identifier: GPL-3.0-only
 
 public class colortest {
+    private static void colorCell(short n) {
+        System.out.printf("\033[48;5;%dm  ", n);
+    }
+    private static void cubeRowPart(short n) {
+        for (short i = n; i < n + 6; i++) colorCell(i);
+        System.out.print("\033[0m");
+    }
+    private static void cubeRow(short n) {
+        cubeRowPart(n);
+        System.out.print("  ");
+        cubeRowPart((short)(n + 36));
+        System.out.print("  ");
+        cubeRowPart((short)(n + 72));
+        System.out.println();
+    }
     public static void main(String[] args) {
         // Print the first 16 colors - these vary by terminal configuration
         System.out.println();
-        for(int i = 0; i < 16; i++) {
-            System.out.print("\033[48;5;" + Integer.toString(i) + "m  ");
-        }
+        for (short i = 0; i < 16; i++) colorCell(i);
         System.out.println("\033[0m\n"); // Double new-line is intentional
 
         // Print the 6 sides of the color cube - these are more standardized
         // but the order is a bit odd, thus the need for this trickery
-        for(int i = 16; i < 52; i+=6) {
-            for(int ii = 0; ii < 6; ii++) {
-                System.out.print("\033[48;5;" + Integer.toString(i+ii) + "m  ");
-            }
-            System.out.print("\033[0m  ");
-            for(int ii = 36; ii < 42; ii++) {
-                System.out.print("\033[48;5;" + Integer.toString(i+ii) + "m  ");
-            }
-            System.out.print("\033[0m  ");
-            for(int ii = 72; ii < 78; ii++) {
-                System.out.print("\033[48;5;" + Integer.toString(i+ii) + "m  ");
-            }
-            System.out.println("\033[0m");
-        }
+        for(short i = 16; i < 52; i += 6) cubeRow(i);
         System.out.println();
-        for(int i = 124; i < 160; i+=6) {
-            for(int ii = 0; ii < 6; ii++) {
-                System.out.print("\033[48;5;" + Integer.toString(i+ii) + "m  ");
-            }
-            System.out.print("\033[0m  ");
-            for(int ii = 36; ii < 42; ii++) {
-                System.out.print("\033[48;5;" + Integer.toString(i+ii) + "m  ");
-            }
-            System.out.print("\033[0m  ");
-            for(int ii = 72; ii < 78; ii++) {
-                System.out.print("\033[48;5;" + Integer.toString(i+ii) + "m  ");
-            }
-            System.out.println("\033[0m");
-        }
+        for(short i = 124; i < 160; i += 6) cubeRow(i);
         System.out.println();
 
         // Finally, the 24 grays
-        for(int i = 232; i < 256; i++) {
-            System.out.print("\033[48;5;"+Integer.toString(i) + "m  ");
-        }
+        for(short i = 232; i < 256; i++) colorCell(i);
         System.out.println("\033[0m\n");
     }
 }
