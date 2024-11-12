a5af87aa0db19f603db48aad74e92115329daaf9
Eli Array Minkoff <eli@planetminkoff.com>
2024-11-12 17:49:45 -0500

    Refactor Lisp
    It's more of the same

--- lisp/colortest.lisp
+++ lisp/colortest.lisp
@@ -8,14 +8,29 @@
 (defvar esc (list (code-char 27)))
 (defvar newline (list (code-char 10)))
 (defvar clearfmt (concatenate 'string esc "[0m"))
-(defun stringofcolor (n)
-  (concatenate 'string esc "[48;5;" (write-to-string n) "m  "))
+(defun color_cell (n)
+  (write-string (concatenate 'string esc "[48;5;" (write-to-string n) "m  ")))
+(defun cube_row_part (n)
+  (let ((i n))
+    (loop
+      (color_cell i)
+      (setq i (+ i 1))
+      (when (< (+ n 5) i) (return))))
+  (write-string clearfmt))
+
+(defun cube_row (n)
+  (cube_row_part n)
+  (write-string "  ")
+  (cube_row_part (+ n 36))
+  (write-string "  ")
+  (cube_row_part (+ n 72))
+  (write-line ""))
 
 ; Print the first 16 colors - these vary by terminal configuration
 (write-line "")
 (let ((i 0))
   (loop
-    (write-string (stringofcolor i))
+    (color_cell i)
     (setq i (+ i 1))
     (when (< 15 i) (return))))
 (write-string (concatenate 'string clearfmt newline newline))
@@ -24,48 +39,13 @@
 ; but the order is a bit odd, thus the need for this trickery
 (let ((i 16))
   (loop
-    (let ((ii 0))
-      (loop
-        (write-string (stringofcolor (+ i ii)))
-        (setq ii (+ ii 1))
-        (when (< 5 ii) (return))))
-    (write-string (concatenate 'string clearfmt "  "))
-    (let ((ii 36))
-      (loop
-        (write-string (stringofcolor (+ i ii)))
-        (setq ii (+ ii 1))
-        (when (< 41 ii) (return))))
-    (write-string (concatenate 'string clearfmt "  "))
-    (let ((ii 72))
-      (loop
-        (write-string (stringofcolor (+ i ii)))
-        (setq ii (+ ii 1))
-        (when (< 77 ii) (return))))
-    (write-string (concatenate 'string clearfmt newline))
+    (cube_row i)
     (setq i (+ i 6))
     (when (< 46 i) (return))))
 (write-line "")
-
 (let ((i 124))
   (loop
-    (let ((ii 0))
-      (loop
-        (write-string (stringofcolor (+ i ii)))
-        (setq ii (+ ii 1))
-        (when (< 5 ii) (return))))
-    (write-string (concatenate 'string clearfmt "  "))
-    (let ((ii 36))
-      (loop
-        (write-string (stringofcolor (+ i ii)))
-        (setq ii (+ ii 1))
-        (when (< 41 ii) (return))))
-    (write-string (concatenate 'string clearfmt "  "))
-    (let ((ii 72))
-      (loop
-        (write-string (stringofcolor (+ i ii)))
-        (setq ii (+ ii 1))
-        (when (< 77 ii) (return))))
-    (write-string (concatenate 'string clearfmt newline))
+    (cube_row i)
     (setq i (+ i 6))
     (when (< 154 i) (return))))
 (write-line "")
@@ -73,7 +53,7 @@
 ; Finally, the 24 grays
 (let ((i 232))
   (loop
-    (write-string (stringofcolor i))
+    (color_cell i)
     (setq i (+ i 1))
     (when (< 255 i) (return))))
 (write-string (concatenate 'string clearfmt newline newline))
