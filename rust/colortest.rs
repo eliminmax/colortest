ace7201841662ccb151b07d8eb275c3dbca00d44
Eli Array Minkoff <eli@planetminkoff.com>
2024-11-14 11:15:50 -0500

    Rust refactor
    * Uses u8 instead of leaving it up to the default type
    * Defines a color_cell function for individual cells
    * Defines a cube_row function for the color cube section
    * Rather than for loops, uses Iterator.for_each(function)

--- rust/colortest.rs
+++ rust/colortest.rs
@@ -2,50 +2,33 @@
 //
 // SPDX-License-Identifier: GPL-3.0-only
 
+fn color_cell(n: u8) {
+    print!("\x1b[48;5;{n}m  ");
+}
+
+fn cube_row(n: u8) {
+    (n..(n + 6)).for_each(color_cell);
+    print!("\x1b[0m  ");
+    ((n + 36)..(n + 42)).for_each(color_cell);
+    print!("\x1b[0m  ");
+    ((n + 72)..(n + 78)).for_each(color_cell);
+    println!("\x1b[0m");
+}
+
 fn main() {
     // Print the first 16 colors - these vary by terminal configuration
     println!();
-    for i in 0..16 {
-        print!("\x1b[48;5;{}m  ", i);
-    }
+    (0u8..16).for_each(color_cell);
     println!("\x1b[0m\n");
 
     // Print the 6 sides of the color cube - these are more standardized
     // but the order is a bit odd, thus the need for this trickery
-    for i in (16..52).step_by(6) {
-        for ii in 0..6 {
-            print!("\x1b[48;5;{}m  ", i + ii);
-        }
-        print!("\x1b[0m  ");
-        for ii in 36..42 {
-            print!("\x1b[48;5;{}m  ", i + ii);
-        }
-        print!("\x1b[0m  ");
-        for ii in 72..78 {
-            print!("\x1b[48;5;{}m  ", i + ii);
-        }
-        println!("\x1b[0m");
-    }
+    (16u8..52).step_by(6).for_each(cube_row);
     println!();
-    for i in (124..160).step_by(6) {
-        for ii in 0..6 {
-            print!("\x1b[48;5;{}m  ", i + ii);
-        }
-        print!("\x1b[0m  ");
-        for ii in 36..42 {
-            print!("\x1b[48;5;{}m  ", i + ii);
-        }
-        print!("\x1b[0m  ");
-        for ii in 72..78 {
-            print!("\x1b[48;5;{}m  ", i + ii);
-        }
-        println!("\x1b[0m");
-    }
+    (124u8..160).step_by(6).for_each(cube_row);
     println!();
 
     // Finally, the 24 grays
-    for i in 232..256 {
-        print!("\x1b[48;5;{}m  ", i);
-    }
+    (232u8..=255).for_each(color_cell);
     println!("\x1b[0m\n")
 }
