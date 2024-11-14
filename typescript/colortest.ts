124e739cfa925cc414128c566ef8c132d682c4fc
Eli Array Minkoff <eli@planetminkoff.com>
2024-11-14 13:13:24 -0500

    Fix typescript shebang
    Should use ts-node, not node

--- typescript/colortest.ts
+++ typescript/colortest.ts
@@ -1,10 +1,10 @@
-#!/usr/bin/env node
+#!/usr/bin/env ts-node
 
-// SPDX-FileCopyrightText: 2022-2023 Eli Array Minkoff
+// SPDX-FileCopyrightText: 2022-2024 Eli Array Minkoff
 //
 // SPDX-License-Identifier: GPL-3.0-only
 
-const util = require('util');
+const util = require('util')
 
 // Print the first 16 colors - these vary by terminal configuration
 process.stdout.write('\n')
