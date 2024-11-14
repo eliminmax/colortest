12deb4031933094a36a71cdc37cc64b4f6e4a4ec
Eli Array Minkoff <eli@planetminkoff.com>
2024-11-14 14:06:56 -0500

    Scala refactor
    One of the more radical refactors I've done.
    Uses a much more functional approach, using the same functions I've been
    using for refactors. Drew influence from the jq and Python
    implementations more than any other, though the Forth implementation did
    influence my approach to ranges.

--- scala/colortest.scala
+++ scala/colortest.scala
@@ -1,53 +1,26 @@
-// SPDX-FileCopyrightText: 2023 Eli Array Minkoff
+// SPDX-FileCopyrightText: 2023-2024 Eli Array Minkoff
 //
 // SPDX-License-Identifier: GPL-3.0-only
 
 object colortest {
+  def colorCell(n: Int) = f"\u001b[48;5;$n%dm  "
+
+  def cubeRowPart(n: Int) = {
+    ((n to (n+5)).map(colorCell).mkString("")) + "\u001b[0m"
+  }
+  def cubeRow(n: Int) = {
+    (0 to 2).map(i => n + (i*36)).map(cubeRowPart).mkString("  ") + "\n"
+  }
   def main(args: Array[String]) = {
     // Print the first 16 colors - these vary by terminal configuration
-    println()
-    for (i <- 0 until 16) {
-      print(f"\u001b[48;5;$i%dm  ")
-    }
-    println("\u001b[0m\n")
+    print("\n" + (0 to 15).map(colorCell).mkString("") + "\u001b[0m\n\n")
 
     // Print the 6 sides of the color cube - these are more standardized
-    // but the order is a bit odd, thus the need for this trickery
-    for (i <- 16 until 52 by 6) {
-      for (ii <- 0 until 6) {
-        print(f"\u001b[48;5;${i+ii}%dm  ")
-      }
-      print("\u001b[0m  ")
-      for (ii <- 36 until 42) {
-        print(f"\u001b[48;5;${i+ii}%dm  ")
-      }
-      print("\u001b[0m  ")
-      for (ii <- 72 until 78) {
-        print(f"\u001b[48;5;${i+ii}%dm  ")
-      }
-      println("\u001b[0m")
-    }
-    println()
-    for (i <- 124 until 160 by 6) {
-      for (ii <- 0 until 6) {
-        print(f"\u001b[48;5;${i+ii}%dm  ")
-      }
-      print("\u001b[0m  ")
-      for (ii <- 36 until 42) {
-        print(f"\u001b[48;5;${i+ii}%dm  ")
-      }
-      print("\u001b[0m  ")
-      for (ii <- 72 until 78) {
-        print(f"\u001b[48;5;${i+ii}%dm  ")
-      }
-      println("\u001b[0m")
-    }
-    println()
+    // but the order is a bit odd, thus the need for the above trickery
+    print((16 to 46 by 6).map(cubeRow).mkString("") + "\n")
+    print((124 to 154 by 6).map(cubeRow).mkString("") + "\n")
 
     // Finally, the 24 grays
-    for (i <- 232 until 256) {
-      print(f"\u001b[48;5;$i%dm  ")
-    }
-    println("\u001b[0m\n")
+    print((232 to 255).map(colorCell).mkString("") + "\u001b[0m\n\n")
   }
 }
