a1f5f0e50999eb1389dc12e8738e3329359f1260
Eli Array Minkoff <eli@planetminkoff.com>
2024-11-13 21:53:51 -0500

    Refactor OCaml
    No longer have separate row_[a-c] functions, and moved function
    declarations to the top of the file. Functions were renamed for
    more consistency with other implementations.

--- ocaml/colortest.ml
+++ ocaml/colortest.ml
@@ -1,10 +1,8 @@
 (*
- * SPDX-FileCopyrightText: 2023 - 2024 Eli Array Minkoff
+ * SPDX-FileCopyrightText: 2023-2024 Eli Array Minkoff
  *
  * SPDX-License-Identifier: GPL-3.0-only
  *)
-
-let colorcell = fun n -> print_string ("\x1b[48;5;" ^ (string_of_int n) ^ "m  ") ;;
 
 (* Based on the language docs - see https://ocaml.org/docs/tour-of-ocaml *)
 let rec range lo hi step =
@@ -16,28 +14,32 @@ let rec range lo hi step =
         lo :: range (lo + step) hi step
 ;;
 
+let color_cell = fun n -> print_string ("\x1b[48;5;" ^ (string_of_int n) ^ "m  ") ;;
+
+let cube_row_part = fun n ->
+    (List.iter color_cell (range (n) (n + 6) 1));
+    print_string("\x1b[0m") ;;
+
+let cube_row = fun n ->
+    cube_row_part n ;
+    print_string "  " ;
+    cube_row_part (n + 36) ;
+    print_string "  " ;
+    cube_row_part (n + 72) ;
+    print_string "\n" ;;
+
 (* Print the first 16 colors - these vary by terminal configuration *)
 print_string "\n";;
-List.iter colorcell (range 0 16 1) ;;
+List.iter color_cell (range 0 16 1) ;;
 print_string "\x1b[0m\n\n" ;;
 
 (* Print the 6 sides of the color cube - these are more standardized,
- * but the order is a bit odd, thus the need for this trickery *)
-let row_a = fun n -> (List.iter colorcell (range n (n + 6) 1)) ;;
-let row_b = fun n -> (List.iter colorcell (range (n + 36) (n + 42) 1)) ;;
-let row_c = fun n -> (List.iter colorcell (range (n + 72) (n + 78) 1)) ;;
-let part_2_row = fun n -> 
-    row_a n ;
-    print_string "\x1b[0m  " ;
-    row_b n ;
-    print_string "\x1b[0m  " ;
-    row_c n ;
-    print_string "\x1b[0m\n" ;;
-List.iter part_2_row (range 16 52 6) ;;
+ * but the order is a bit odd, thus the need for the above trickery *)
+List.iter cube_row (range 16 52 6) ;;
 print_string "\n" ;;
-List.iter part_2_row (range 124 160 6) ;;
+List.iter cube_row (range 124 160 6) ;;
 print_string "\n" ;;
 
 (* Finally, the 24 grays *)
-List.iter colorcell (range 232 256 1) ;;
+List.iter color_cell (range 232 256 1) ;;
 print_string "\x1b[0m\n\n" ;;
