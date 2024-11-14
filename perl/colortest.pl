2dcc0ff90897d4cd39b655ee8bb6db1a023af710
Eli Array Minkoff <eli@planetminkoff.com>
2024-11-13 23:32:45 -0500

    Perl refactor time

--- perl/colortest.pl
+++ perl/colortest.pl
@@ -1,54 +1,50 @@
 #!/usr/bin/env perl
 
-# SPDX-FileCopyrightText: 2022 Eli Array Minkoff
+# SPDX-FileCopyrightText: 2022-2024 Eli Array Minkoff
 #
 # SPDX-License-Identifier: GPL-3.0-only
 
 use strict;
 use warnings;
 
-# Print the first 16 colors - these vary by terminal configuration
-printf "\n" ;
-for (my $i = 0; $i < 16; $i++) {
-    printf "\e[48;5;%dm  ", $i ;
+sub color_cell {
+    printf "\e[48;5;%dm  ", $_[0];
 }
-printf "\e[0m\n\n";
+
+sub cube_row_part {
+    for (my $i = $_[0]; $i < $_[0] + 6; $i++) {
+        color_cell $i;
+    }
+}
+sub cube_row {
+    cube_row_part $_[0];
+    print "\x1b[0m  ";
+    cube_row_part ($_[0] + 36);
+    print "\x1b[0m  ";
+    cube_row_part ($_[0] + 72);
+    print "\x1b[0m\n";
+}
+
+# Print the first 16 colors - these vary by terminal configuration
+print "\n" ;
+for (my $i = 0; $i < 16; $i++) {
+    color_cell $i
+}
+print "\e[0m\n\n";
 
 # Print the 6 sides of the color cube - these are more standardized
 # but the order is a bit odd, thus the need for this trickery
 for (my $i = 16; $i < 52; $i+=6) {
-    for (my $ii = 0; $ii < 6; $ii++) {
-        printf "\e[48;5;%dm  ", ($i+$ii);
-    }
-    printf "\e[0m  ";
-    for (my $ii = 36; $ii < 42; $ii++) {
-        printf "\e[48;5;%dm  ", ($i+$ii);
-    }
-    printf "\e[0m  ";
-    for (my $ii = 72; $ii < 78; $ii++) {
-        printf "\e[48;5;%dm  ", ($i+$ii);
-    }
-    printf "\e[0m\n";
+    cube_row $i
 }
-printf "\n";
+print "\n";
 for (my $i = 124; $i < 160; $i+=6) {
-    for (my $ii = 0; $ii < 6; $ii++) {
-        printf "\e[48;5;%dm  ", ($i+$ii);
-    }
-    printf "\e[0m  ";
-    for (my $ii = 36; $ii < 42; $ii++) {
-        printf "\e[48;5;%dm  ", ($i+$ii);
-    }
-    printf "\e[0m  ";
-    for (my $ii = 72; $ii < 78; $ii++) {
-        printf "\e[48;5;%dm  ", ($i+$ii);
-    }
-    printf "\e[0m\n";
+    cube_row $i
 }
-printf "\n";
+print "\n";
 
 # Finally, the 24 grays
 for (my $i = 232; $i < 256; $i++) {
-    printf "\e[48;5;%dm  ", $i;
+    color_cell $i
 }
-printf "\e[0m\n\n"
+print "\e[0m\n\n"
