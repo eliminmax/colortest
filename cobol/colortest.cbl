eaa7fe334796fcdf82e24bcbc854676df9add31e
Eli Array Minkoff <eli@planetminkoff.com>
2024-11-28 13:54:31 -0500

    Use TEST BEFORE VARYING in COBOL

--- cobol/colortest.cbl
+++ cobol/colortest.cbl
@@ -14,49 +14,48 @@
       * Number Display
            01 ND PICTURE ZZ9.
       * Primary iterator value
-           01 I PICTURE 999 VALUE 0.
+           01 I PICTURE 999.
       * ColorCube-Row-Part iterator value
            01 ICRP PICTURE 999.
        PROCEDURE DIVISION.
        MAIN-PARA.
       *    Print the first 16 colors - these vary by terminal config
            DISPLAY LINE-FEED WITH NO ADVANCING
-           PERFORM COLORCELL-PARA WITH TEST BEFORE UNTIL I=16
+           PERFORM COLORCELL-PARA TEST BEFORE VARYING I
+           FROM 0 BY 1 UNTIL I=16
            DISPLAY ESC'[0m'LINE-FEED
       *    Print the 6 sides of the color cube - these are more
       *    standardized but the order is a bit odd, thus the need for
       *    the below trickery
-           PERFORM COLORCUBE-ROW-PARA WITH TEST BEFORE UNTIL I=52
+           PERFORM COLORCUBE-ROW-PARA TEST BEFORE VARYING I
+           FROM 16 BY 6 UNTIL I=52
            DISPLAY LINE-FEED WITH NO ADVANCING
-           MOVE 124 TO I
-           PERFORM COLORCUBE-ROW-PARA WITH TEST BEFORE UNTIL I=160
+           PERFORM COLORCUBE-ROW-PARA TEST BEFORE VARYING I
+           FROM 124 BY 6 UNTIL I=160
            DISPLAY LINE-FEED WITH NO ADVANCING
       *    Finally, the 24 grays
-           MOVE 232 TO I
-           PERFORM COLORCELL-PARA WITH TEST BEFORE UNTIL I=256
+           PERFORM COLORCELL-PARA TEST BEFORE VARYING I
+           FROM 232 BY 1 UNTIL I=256
            DISPLAY ESC"[0m"LINE-FEED
            STOP RUN.
 
        COLORCELL-PARA.
            MOVE I TO ND
-           DISPLAY ESC"[48;5;"FUNCTION TRIM(ND)"m  " WITH NO ADVANCING
-           ADD 1 TO I.
+           DISPLAY ESC"[48;5;"FUNCTION TRIM(ND)"m  " WITH NO ADVANCING.
 
        COLORCUBE-ROW-PARA.
-           MOVE 0 TO ICRP
-           PERFORM CUBE-ROW-PART-PARA WITH TEST BEFORE UNTIL ICRP=6
+           PERFORM CUBE-ROW-PART-PARA TEST BEFORE VARYING ICRP
+           FROM 0 BY 1 UNTIL ICRP=6
            DISPLAY ESC"[0m  " WITH NO ADVANCING
-           MOVE 36 TO ICRP
-           PERFORM CUBE-ROW-PART-PARA WITH TEST BEFORE UNTIL ICRP=42
+           PERFORM CUBE-ROW-PART-PARA TEST BEFORE VARYING ICRP
+           FROM 36 by 1 UNTIL ICRP=42
            DISPLAY ESC"[0m  " WITH NO ADVANCING
-           MOVE 72 TO ICRP
-           PERFORM CUBE-ROW-PART-PARA WITH TEST BEFORE UNTIL ICRP=78
-           DISPLAY ESC"[0m"
-           ADD 6 TO I.
-      * Name abreviated to fit within above line limits
+           PERFORM CUBE-ROW-PART-PARA TEST BEFORE VARYING ICRP
+           FROM 72 BY 1 UNTIL ICRP=78
+           DISPLAY ESC"[0m".
+
        CUBE-ROW-PART-PARA.
            ADD ICRP TO I
            MOVE I TO ND
-           DISPLAY ESC"[48;5;"FUNCTION TRIM(ND)"m  " WITH NO ADVANCING
-           SUBTRACT ICRP FROM I
-           ADD 1 TO ICRP.
+           PERFORM COLORCELL-PARA.
+           SUBTRACT ICRP FROM I.
