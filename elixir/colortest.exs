d2aa9f8b5eb51d2b943e3abf023708cb24e16354
Eli Array Minkoff <eli@planetminkoff.com>
2025-01-22 13:49:07 -0500

    Remove unnecessary parens from IO.write

--- elixir/colortest.exs
+++ elixir/colortest.exs
@@ -1,20 +1,20 @@
 #!/usr/bin/env elixir
-# SPDX-FileCopyrightText: 2024 Eli Array Minkoff
+# SPDX-FileCopyrightText: 2024 - 2025 Eli Array Minkoff
 #
 # SPDX-License-Identifier: GPL-3.0-only
 
 color_cell = fn n -> IO.write "\x1b[48;5;" <> (Integer.to_string n) <> "m  "  end
 cube_row_part = fn n -> Enum.map(n..(n+5), color_cell); end
 cube_row = fn n ->
-  cube_row_part.(n); IO.write("\x1b[0m  ")
-  cube_row_part.(n+36); IO.write("\x1b[0m  ")
-  cube_row_part.(n+72); IO.write("\x1b[0m\n")
+  cube_row_part.(n);    IO.write "\x1b[0m  "
+  cube_row_part.(n+36); IO.write "\x1b[0m  "
+  cube_row_part.(n+72); IO.write "\x1b[0m\n"
 end
 
 # Print the first 16 colors - these vary by terminal configuration
 IO.write "\n"
 Enum.map(0..15, color_cell)
-IO.write("\x1b[0m\n\n")
+IO.write "\x1b[0m\n\n"
 
 # Print the 6 sides of the color cube - these are more standardized
 # but the order is a bit odd, thus the need for the above trickery
@@ -25,4 +25,4 @@ IO.write "\n"
 
 # finally, the 24 grays
 Enum.map(232..255, color_cell)
-IO.write("\x1b[0m\n\n")
+IO.write "\x1b[0m\n\n"
