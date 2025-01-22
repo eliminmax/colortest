bef46b77275fcf31a9123f10c1f6b7415d7ae5ec
Eli Array Minkoff <eli@planetminkoff.com>
2025-01-22 11:00:06 -0500

    Simplify layout of csharp/colortest.cs
    * Remove accidental indentation
    * remove unnecessary brackets

--- csharp/colortest.cs
+++ csharp/colortest.cs
@@ -8,15 +8,13 @@ class colortest {
         Console.Write("\x1b[48;5;{0}m  ", n);
     }
     private static void CubeRowPart(uint n) {
-        for(uint i = n; i < n + 6; i++) {
-            ColorCell(i);
-        }
+        for(uint i = n; i < n + 6; i++) ColorCell(i);
         Console.Write("\x1b[0m");
     }
     private static void CubeRow(uint n) {
         CubeRowPart(n);
         Console.Write("  ");
-            CubeRowPart(n + 36);
+        CubeRowPart(n + 36);
         Console.Write("  ");
         CubeRowPart(n + 72);
         Console.WriteLine();
@@ -24,26 +22,18 @@ class colortest {
     // Print the first 16 colors - these vary by terminal configuration
     public static void Main(string[] args) {
         Console.WriteLine();
-        for (uint i = 0; i < 16; i++) {
-            ColorCell(i);
-        }
+        for (uint i = 0; i < 16; i++) ColorCell(i);
         Console.WriteLine("\x1b[0m\n"); // Double new-line is intentional
 
         // Print the 6 sides of the color cube - these are more standardized
         // but the order is a bit odd, thus the need for the above trickery
-        for (uint i = 16; i < 52; i+=6) {
-            CubeRow(i);
-        }
+        for (uint i = 16; i < 52; i+=6) CubeRow(i);
         Console.WriteLine();
-        for (uint i = 124; i < 160; i+=6) {
-            CubeRow(i);
-        }
+        for (uint i = 124; i < 160; i+=6) CubeRow(i);
         Console.WriteLine();
 
         // Finally, the 24 grays
-        for(uint i = 232; i < 256; i++) {
-            ColorCell(i);
-        }
+        for (uint i = 232; i < 256; i++) ColorCell(i);
         Console.WriteLine("\x1b[0m\n");
     }
 }
