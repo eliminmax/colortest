bfbb8c3b57438dff7e0d8d8f839761a0e7d3069b
Eli Array Minkoff <eli@planetminkoff.com>
2025-12-24 18:41:52 -0500

    Update zig implementation for Writergate
    Buffered printing to stdout in Zig now requires more explicit buffer
    usage, so the Zig implementation now uses that, and the version of Zig
    installed by `RUNNERS/install-deps.sh` can be updated to the latest
    release.
    
    see https://github.com/ziglang/zig/pull/24329

--- zig/colortest.zig
+++ zig/colortest.zig
@@ -3,35 +3,39 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
 const std = @import("std");
-const stdout_writer = std.io.getStdOut().writer();
+const Writer = std.Io.Writer;
 
-fn colorCell(n: u8) !void {
-    try stdout_writer.print("\x1b[48;5;{d}m  ", .{n});
+fn colorCell(n: u8, writer: *Writer) !void {
+    try writer.print("\x1b[48;5;{d}m  ", .{n});
 }
 
-fn cubeRowPart(n: u8) !void {
+fn cubeRowPart(n: u8, writer: *Writer) !void {
     for (n..(n + 6)) |i| {
         // Zig for(range) syntax really should allow int types other than usize
-        try colorCell(@intCast(i));
+        try colorCell(@intCast(i), writer);
     }
 }
 
-fn cubeRow(n: u8) !void {
-    try cubeRowPart(n);
-    try stdout_writer.print("\x1b[0m  ", .{});
-    try cubeRowPart(n + 36);
-    try stdout_writer.print("\x1b[0m  ", .{});
-    try cubeRowPart(n + 72);
-    try stdout_writer.print("\x1b[0m\n", .{});
+fn cubeRow(n: u8, writer: *Writer) !void {
+    try cubeRowPart(n, writer);
+    try writer.writeAll("\x1b[0m  ");
+    try cubeRowPart(n + 36, writer);
+    try writer.writeAll("\x1b[0m  ");
+    try cubeRowPart(n + 72, writer);
+    try writer.writeAll("\x1b[0m\n");
 }
 
 pub fn main() !void {
     // Print the first 16 colors - these vary by terminal configuration
-    try stdout_writer.print("\n", .{});
+    var buffer: [4096]u8 = undefined;
+    var stdout_writer = std.fs.File.stdout().writer(&buffer);
+    var writer = &stdout_writer.interface;
+
+    try writer.writeAll("\n");
     for (0..16) |i| {
-        try colorCell(@intCast(i));
+        try colorCell(@intCast(i), writer);
     }
-    try stdout_writer.print("\x1b[0m\n\n", .{});
+    try writer.writeAll("\x1b[0m\n\n");
 
     // Print the 6 sides of the color cube - these are more standardized,
     //  but the order is a bit odd, thus the need for this trickery
@@ -40,18 +44,19 @@ pub fn main() !void {
         // this should be in its own scope
         var i: u8 = 16;
         while (i < 52) : (i += 6) {
-            try cubeRow(i);
+            try cubeRow(i, writer);
         }
-        try stdout_writer.print("\n", .{});
+        try writer.writeAll("\n");
         i = 124;
         while (i < 160) : (i += 6) {
-            try cubeRow(i);
+            try cubeRow(i, writer);
         }
-        try stdout_writer.print("\n", .{});
+        try writer.writeAll("\n");
     }
     // Finally, the 24 grays
     for (232..256) |i| {
-        try colorCell(@intCast(i));
+        try colorCell(@intCast(i), writer);
     }
-    try stdout_writer.print("\x1b[0m\n\n", .{});
+    try writer.writeAll("\x1b[0m\n\n");
+    try writer.flush();
 }
