966beb6cc5c7693b3b178912bb71ed0c736e0848
Eli Array Minkoff <eli@planetminkoff.com>
2025-12-26 23:24:47 -0500

    Rust: Remove unneeded explicit `u8` size suffixes

--- rust/colortest.rs
+++ rust/colortest.rs
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2022 - 2024 Eli Array Minkoff
+// SPDX-FileCopyrightText: 2022 - 2025 Eli Array Minkoff
 //
 // SPDX-License-Identifier: GPL-3.0-only
 
@@ -18,17 +18,17 @@ fn cube_row(n: u8) {
 fn main() {
     // Print the first 16 colors - these vary by terminal configuration
     println!();
-    (0u8..16).for_each(color_cell);
+    (0..16).for_each(color_cell);
     println!("\x1b[0m\n");
 
     // Print the 6 sides of the color cube - these are more standardized
     // but the order is a bit odd, thus the need for this trickery
-    (16u8..52).step_by(6).for_each(cube_row);
+    (16..52).step_by(6).for_each(cube_row);
     println!();
-    (124u8..160).step_by(6).for_each(cube_row);
+    (124..160).step_by(6).for_each(cube_row);
     println!();
 
     // Finally, the 24 grays
-    (232u8..=255).for_each(color_cell);
+    (232..=255).for_each(color_cell);
     println!("\x1b[0m\n")
 }
