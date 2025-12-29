e7e3ae1222aac06c0a1b3bed65876d037d54d54a
Eli Array Minkoff <eli@planetminkoff.com>
2025-12-29 14:32:06 -0500

    Avoid using endl in C++ to reduce needed writes
    Outputting endl flushes the buffer immediately, but a literal "\n"
    doesn't. This reduces the total number of needed writes to 1.

--- cpp/colortest.cpp
+++ cpp/colortest.cpp
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2022 - 2024 Eli Array Minkoff
+// SPDX-FileCopyrightText: 2022 - 2025 Eli Array Minkoff
 //
 // SPDX-License-Identifier: GPL-3.0-only
 
@@ -19,26 +19,25 @@ static void cube_row(int n) {
     cube_row_part(n + 36);
     cout << "\x1b[0m  ";
     cube_row_part(n + 72);
-    cout << "\x1b[0m" << endl;
+    cout << "\x1b[0m\n";
 }
 
 int main() {
     // Print the first 16 colors - these vary by terminal configuration
-    cout << endl;
+    cout << "\n";
     for (int i = 0; i < 16; i++) color_cell(i);
-    // use one literal '\n' and one endl to only flush output once
-    cout << "\x1b[0m\n" << endl;
+    cout << "\x1b[0m\n\n";
 
     // Print the 6 sides of the color cube - these are more standardized
     // but the order is a bit odd, thus the need for the above trickery
     for (int i = 16; i < 52; i += 6) cube_row(i);
-    cout << endl;
+    cout << '\n';
     for (int i = 124; i < 160; i += 6) cube_row(i);
-    cout << endl;
+    cout << '\n';
 
     // Finally, the 24 grays
     for (int i = 232; i < 256; i++) color_cell(i);
-    cout << "\x1b[0m\n" << endl;
+    cout << "\x1b[0m\n\n";
 
     return 0;
 }
