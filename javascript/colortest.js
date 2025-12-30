78197a2358406a4bc2434216204370bf9a173e49
Eli Array Minkoff <eli@planetminkoff.com>
2025-12-29 21:36:19 -0500

    Use modern JS let over obsolete var

--- javascript/colortest.js
+++ javascript/colortest.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-// SPDX-FileCopyrightText: 2022 - 2024 Eli Array Minkoff
+// SPDX-FileCopyrightText: 2022 - 2025 Eli Array Minkoff
 //
 // SPDX-License-Identifier: GPL-3.0-only
 
@@ -13,7 +13,7 @@ function color_cell(n) {
 }
 
 function cube_row_part(n) {
-    for (var i = n; i < n + 6; i++) color_cell(i)
+    for (let i = n; i < n + 6; i++) color_cell(i)
 }
 
 function cube_row(n) {
@@ -27,16 +27,16 @@ function cube_row(n) {
 
 // Print the first 16 colors - these vary by terminal configuration
 process.stdout.write('\n')
-for (var i = 0; i < 16; i++) color_cell(i)
+for (let i = 0; i < 16; i++) color_cell(i)
 process.stdout.write('\u001b[0m\n\n')
 
 // Print the 6 sides of the color cube - these are more standardized
 // but the order is a bit odd, thus the need for the above trickery
-for (var i = 16; i < 52; i+=6) cube_row(i)
+for (let i = 16; i < 52; i+=6) cube_row(i)
 process.stdout.write('\n')
-for (var i = 124; i < 160; i+=6) cube_row(i)
+for (let i = 124; i < 160; i+=6) cube_row(i)
 process.stdout.write('\n')
 
 // Finally, the 24 grays
-for (var i = 232; i < 256; i++) color_cell(i)
+for (let i = 232; i < 256; i++) color_cell(i)
 process.stdout.write('\u001b[0m\n\n')
